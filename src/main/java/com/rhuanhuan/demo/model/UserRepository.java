36643567c990a8ef423b50ceb05f857de464a25e
rhuanhuan <1210098674@qq.com>
2018-11-11 21:27:03 +0800

    add db config, and User ENTITY and DAO

---
+++ src/main/java/com/rhuanhuan/demo/model/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rhuanhuan.demo.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  User findByUserName(String userName);
+
+  User findByUserNameOrEmail(String username, String email);
+}
